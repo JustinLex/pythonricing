20a6be07ccd5e522f1ce2559fd2a35fce704fe75
Justin Lex-Hammarskjöld <justin@stakater.com>
2022-05-09 16:33:05 +0200

    Implement app logic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import asyncio
+from typing import Optional
 
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse, Response
@@ -7,14 +7,13 @@ from starlette.requests import Request
 
 from loguru import logger
 
-
 import redis.asyncio as redis
 
 from .logs import init_logger
 
 
 # Connection object for redis
-redis_conn = None
+redis_conn: Optional[redis.Redis] = None
 
 
 # Set up loguru and inject it so that it intercepts all stdlib and uvicorn logging
@@ -24,11 +23,37 @@ init_logger()
 # Our one and only Route
 async def homepage(request: Request) -> Response:
 
-    # Log current request number
-    # Using loguru kwargs to add the "request_count" key-value to the "extra" dict in the JSON log line
-    logger.info("Request #{request_count}", request_count=0)
+    # Get current count if exists
+    redis_request_count: Optional[str] = await redis_conn.get('request_count')
 
-    return JSONResponse({'hello': 'world'})
+    # convert redis_request_count to int
+    if redis_request_count is None:  # Initialize counter if we were the first ones here
+        request_count: int = 0
+    else:
+        request_count: int = int(redis_request_count)
+
+    # Normal operation, first 100 requests. Update counter asap, Log and response.
+    if request_count < 100:
+        request_count += 1
+        await redis_conn.set('request_count', str(request_count))
+
+        # Log current request number
+        # Using loguru kwargs to add the "request_count" key-value to the "extra" dict in the JSON log line
+        logger.info("Request #{request_count}", request_count=request_count)
+
+        return JSONResponse({
+            'hello': 'world',
+            'request_num': request_count,
+        })
+
+    # We already hit 100 requests, log and response
+    else:
+        logger.warning("Received a request but we already hit 100 requests!")
+
+        return JSONResponse({
+            'Go': 'away!',
+            'request_num': request_count,
+        })
 
 
 # Starlette entrypoint
@@ -38,6 +63,7 @@ app = Starlette(debug=True, routes=[
 
 
 # Connect to redis once Starlette has started
+# (Also ensures redis server is running before we handle requests)
 @app.on_event("startup")
 async def connect_redis() -> None:
     global redis_conn
